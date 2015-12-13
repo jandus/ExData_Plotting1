6bec2a2289315f8c71b411ce45c11d072b584d02
jandus <jalejandro.adu@gmail.com>
2015-12-13 13:05:18 -0500

    code formating change

--- plot3.R
+++ plot3.R
@@ -5,18 +5,20 @@ library(dplyr)
 par(mfrow=c(1,1)) # set to 1 row and 1 column
 
 # Extract & Transform file into a data set
-house_consumption<- read.table("household_power_consumption.txt", header = TRUE, sep = ';', dec='.', colClasses=c(character(), character(), numeric(), numeric(), numeric(), numeric(), numeric(), numeric()), na.strings = "?",row.names = NULL)
-house_consumption$Date<-as.Date(house_consumption$Date, "%d/%m/%Y")
-house_consumption<-subset(house_consumption, Date==as.Date("2007-02-01") | Date==as.Date("2007-02-02"))
-house_consumption$datetime<-strptime(paste(house_consumption$Date, house_consumption$Time), "%Y-%m-%d %H:%M:%S")
+house_consumption <- read.table("household_power_consumption.txt", header = TRUE, sep = ';', dec='.',
+                               colClasses=c(character(), character(), numeric(), numeric(), numeric(), numeric(), numeric(), numeric()),
+                               na.strings = "?",row.names = NULL)
+house_consumption$Date <- as.Date(house_consumption$Date, "%d/%m/%Y")
+house_consumption <- subset(house_consumption, Date == as.Date("2007-02-01") | Date == as.Date("2007-02-02"))
+house_consumption$datetime <- strptime(paste(house_consumption$Date, house_consumption$Time), "%Y-%m-%d %H:%M:%S")
 
 
 # Build Plot graph
 png(filename = "Plot3.png", width = 480, height = 480, units = "px", 
     pointsize = 12, bg = "transparent")
-plot(house_consumption$datetime,house_consumption$Sub_metering_1, type="n", xlab="", ylab="Energy sub metering")
-    lines(house_consumption$datetime,house_consumption$Sub_metering_1, col="black")
-    lines(house_consumption$datetime,house_consumption$Sub_metering_2, col="brown")
-    lines(house_consumption$datetime,house_consumption$Sub_metering_3, col="navyblue")
-    legend("topright", pch="-", col=c("black","brown","navyblue"), legend=c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"), pt.cex=2, cex=0.7)
+plot(house_consumption$datetime,house_consumption$Sub_metering_1, type = "n", xlab = "", ylab = "Energy sub metering")
+    lines(house_consumption$datetime,house_consumption$Sub_metering_1, col = "black")
+    lines(house_consumption$datetime,house_consumption$Sub_metering_2, col = "brown")
+    lines(house_consumption$datetime,house_consumption$Sub_metering_3, col = "navyblue")
+    legend("topright", pch = "-", col = c("black","brown","navyblue"), legend = c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"), pt.cex = 2, cex = 0.7)
 dev.off()
--- plot4.R
+++ plot4.R
@@ -6,10 +6,12 @@ par(mfrow=c(2,2)) # set to show different graphs in same image
 
 
 # Extract & Transform file into a data set
-house_consumption<- read.table("household_power_consumption.txt", header = TRUE, sep = ';', dec='.', colClasses=c(character(), character(), numeric(), numeric(), numeric(), numeric(), numeric(), numeric()), na.strings = "?",row.names = NULL)
-house_consumption$Date<-as.Date(house_consumption$Date, "%d/%m/%Y")
-house_consumption<-subset(house_consumption, Date==as.Date("2007-02-01") | Date==as.Date("2007-02-02"))
-house_consumption$datetime<-strptime(paste(house_consumption$Date, house_consumption$Time), "%Y-%m-%d %H:%M:%S")
+house_consumption <- read.table("household_power_consumption.txt", header = TRUE, sep = ';', dec='.', 
+                               colClasses=c(character(), character(), numeric(), numeric(), numeric(), numeric(), numeric(), numeric()),
+                               na.strings = "?",row.names = NULL)
+house_consumption$Date <- as.Date(house_consumption$Date, "%d/%m/%Y")
+house_consumption <- subset(house_consumption, Date == as.Date("2007-02-01") | Date == as.Date("2007-02-02"))
+house_consumption$datetime <- strptime(paste(house_consumption$Date, house_consumption$Time), "%Y-%m-%d %H:%M:%S")
 
 
 # Build Plot graph
@@ -17,16 +19,18 @@ png(filename = "Plot4.png", width = 480, height = 480, units = "px",
     pointsize = 12, bg = "transparent")
 par(mfrow=c(2,2))
 #graph 1
-plot(house_consumption$datetime,house_consumption$Global_active_power, type="l", col ="red", bg ="transparent", xlab="", ylab="Global Active Power")
+plot(house_consumption$datetime,house_consumption$Global_active_power, type = "l", col = "red", bg = "transparent",
+     xlab = "", ylab = "Global Active Power")
 #graph 2
-plot(house_consumption$datetime,house_consumption$Voltage, type="l", col ="black", bg ="transparent", xlab="datetime", ylab="Voltage")
+plot(house_consumption$datetime,house_consumption$Voltage, type = "l", col = "black", bg = "transparent",
+     xlab = "datetime", ylab = "Voltage")
 #grap 3
-plot(house_consumption$datetime,house_consumption$Sub_metering_1, type="n", xlab="", ylab="Energy sub metering")
-lines(house_consumption$datetime,house_consumption$Sub_metering_1, col="black")
-lines(house_consumption$datetime,house_consumption$Sub_metering_2, col="brown")
-lines(house_consumption$datetime,house_consumption$Sub_metering_3, col="navyblue")
-legend("topright", pch="-", col=c("black","brown","navyblue"), legend=c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"), box.lt=0, pt.cex=1.4, cex=1)
+plot(house_consumption$datetime,house_consumption$Sub_metering_1, type = "n", xlab = "", ylab = "Energy sub metering")
+lines(house_consumption$datetime,house_consumption$Sub_metering_1, col = "black")
+lines(house_consumption$datetime,house_consumption$Sub_metering_2, col = "brown")
+lines(house_consumption$datetime,house_consumption$Sub_metering_3, col = "navyblue")
+legend("topright", pch = "-", col = c("black","brown","navyblue"), legend = c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"), box.lt = 0, pt.cex = 1.4, cex = 1)
 #grap 4 - using name of field as x label
-with(house_consumption, plot(datetime,Global_reactive_power, type="l", col ="black", bg ="transparent"))
+with(house_consumption, plot(datetime,Global_reactive_power, type = "l", col = "black", bg = "transparent"))
 dev.off()
 
